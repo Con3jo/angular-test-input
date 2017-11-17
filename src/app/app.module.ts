38cd57047de46d0cb8e945999afa1046b9113cd4
Fernanda Ramírez <fernanda.ramirez.olea@gmail.com>
2017-11-17 00:29:57 -0300

    New component: table-group

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { GroupTableComponent } from './group-table/group-table.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroupTableComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/group-table/group-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-group-table',
+  templateUrl: './group-table.component.html',
+  styleUrls: ['./group-table.component.css']
+})
+export class GroupTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
